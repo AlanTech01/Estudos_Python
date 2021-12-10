d604f0c28fe917188e9fcb7bae3905142dfac9bd
ALAN RIBEIRO ALVES <94498033+AlanTech01@users.noreply.github.com>
2021-12-09 23:42:31 -0300

    Desafio 2
    Passos iniciais usando entradas e  print

---
+++ Desadio2.py
@@ -0,0 +1,5 @@
+Dia = input('Dia do seu nascimento? ')
+Mes = input('Mês do seu nascimento? ')
+Ano = input('Ano do seu nascimento? ')
+print('Você nasceu no dia', Dia, 'de', Mes,'de' , Ano,'. Certo?')
+
